1c8c5140a09638eee2970bf1be40a23a2bbad7f1
Romu Tiwari <tromu98@gmail.com>
2024-12-14 00:09:05 +0530

    Version 2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Welcome to Flask</h2><p>This is the homepage</p>"
+    return "<h1>Welcome to Flask Version 2</h2><p>This is the homepage</p>"
 
 @app.route('/about')
 def aboutus():
