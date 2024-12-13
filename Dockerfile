d18edabdbba87f309dd92f6e07249990ab2cfdf2
Romu Tiwari <blazerrt@Romus-MacBook-Pro.local>
2024-12-13 21:01:29 +0530

    Simple Flask App

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM python:3.11-slim as builder
+
+WORKDIR /app
+
+COPY requirements.txt .
+
+RUN pip install --no-cache-dir --prefix=/install -r requirements.txt
+
+COPY . ./
+
+FROM gcr.io/distroless/python3
+
+WORKDIR /app
+
+COPY --from=builder /install /usr/local
+COPY --from=builder /app /app
+
+EXPOSE 5000
+
+CMD [ "app.py" ]
---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "<h1>Welcome to Flask</h2><p>This is the homepage</p>"
+
+@app.route('/about')
+def aboutus():
+    return "<h1>This is about us page</h1><p>This is a simple flask application</p>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
